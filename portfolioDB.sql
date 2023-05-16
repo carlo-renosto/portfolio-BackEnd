cfddb53fda775f929573e6d1d767a9366d63df6f
carlo-renosto <130240783+carlo-renosto@users.noreply.github.com>
2023-05-15 21:22:44 -0300

    Actualizados DER, UML y script SQL

--- portfolioDB.sql
+++ portfolioDB.sql
@@ -19,7 +19,7 @@ CREATE TABLE Persona(
 
 CREATE TABLE Institucion(
 	Id int, 
-    Nombre varchar(30) NOT NULL,
+    Nombre varchar(50) NOT NULL,
     Ubicacion varchar(20) NOT NULL,
     
     CONSTRAINT PK_ID_I PRIMARY KEY(Id)
@@ -57,13 +57,15 @@ CREATE TABLE ExperienciaLaboral(
 );
 
 CREATE TABLE Educacion(
+	Id int,
 	IdInstitucion int,
     IdPersona int,
-    NombreCarrera varchar(30) NOT NULL,
+    NombreCarrera varchar(50) NOT NULL,
     AñoInicio int NOT NULL,
     AñoFin int NOT NULL,
+    Estado bit,
     
-    CONSTRAINT PK_ID_E PRIMARY KEY(IdInstitucion),
+    CONSTRAINT PK_ID_E PRIMARY KEY(Id),
     CONSTRAINT FK_IDI_E FOREIGN KEY(IdInstitucion) REFERENCES Institucion(Id),
     CONSTRAINT FK_IDP_E FOREIGN KEY(IdPersona) REFERENCES Persona(Id)
 );
@@ -71,8 +73,8 @@ CREATE TABLE Educacion(
 CREATE TABLE Proyecto(
 	Id int,
     IdPersona int,
-    Nombre varchar(20) NOT NULL,
-    Descripcion varchar(50) NOT NULL,
+    Nombre varchar(50) NOT NULL,
+    Descripcion varchar(100) NOT NULL,
     FechaRealizacion char(10) NOT NULL,
     
 	CONSTRAINT PK_ID_P PRIMARY KEY(Id),
@@ -91,9 +93,18 @@ CREATE TABLE TecnologiasXPersona(
 CREATE PROCEDURE RegistrarUsuario(iId int, iNombre char(20), iContra char(20), iEsAdmin boolean)
 	INSERT INTO Usuario VALUES(iId, iNombre, iContra, iEsAdmin);
 
-CALL RegistrarUsuario(1, "Admin", "argprograma", true)
+CALL RegistrarUsuario(1, "Admin", "argprograma", true);
 
+-- Inserción de registros
 
+INSERT INTO Persona VALUES(1, "Carlo", "Renosto", 20, "Informático (Estudiante)");
+INSERT INTO Institucion VALUES(1, "Universidad Tecnológica Nacional", "Tigre");
+INSERT INTO Institucion VALUES(2, "Laboratorio de Idiomas UBA", "CABA");
+INSERT INTO Institucion VALUES(3, "Escuela Don Orione", "Tigre");
+INSERT INTO Educacion VALUES(1, 3, 1, "Nivel Secundario", 2015, 2020, 0);
+INSERT INTO Educacion VALUES(2, 1, 1, "Técnico Universitario en Programación", 2021, 2023, 1);
+INSERT INTO Educacion VALUES(3, 2, 1, "Inglés - Nivel 9 (B1)", 2022, 2022, 0);
+INSERT INTO Educacion VALUES(4, 2, 1, "Inglés - Nivel 10", 2023, 2023, 1);
 
 
 
